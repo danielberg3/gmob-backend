38a68f77814187309fe948e793ffe85bf20ecc97
Felipe-MML <felipemaras20@gmail.com>
2025-07-22 22:36:00 -0300

    Correção de paginação e Bugs

--- src/transacoes/transacoes.controller.ts
+++ src/transacoes/transacoes.controller.ts
@@ -1,26 +1,41 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Request, UseGuards } from "@nestjs/common";
-import { CreateTransacoesDto } from "./dto/create-transacoes.dto";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateTransacoesDto } from './dto/create-transacoes.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { TransacoesService } from "./transacoes.service";
-import { get } from "http";
+import { TransacoesService } from './transacoes.service';
+import { get } from 'http';
 
 @Controller('transacoes')
 @UseGuards(JwtAuthGuard)
 export class TransacoesController {
-    constructor(private readonly transacoesService: TransacoesService) {}
+  constructor(private readonly transacoesService: TransacoesService) {}
 
-    @Post()
-    create(@Body() createTransacoesDto: CreateTransacoesDto, @Request() req) {
-        return this.transacoesService.create(createTransacoesDto, req.user);
-    }
+  @Post()
+  create(@Body() createTransacoesDto: CreateTransacoesDto, @Request() req) {
+    return this.transacoesService.create(createTransacoesDto, req.user);
+  }
 
-    @Get()
-    findAll(@Request() req) {
-        return this.transacoesService.findAll(req.user);
-    }   
+  @Get()
+  findAll(
+    @Request() req,
+    @Query('page', ParseIntPipe) page: number = 1,
+    @Query('limit', ParseIntPipe) limit: number = 10,
+  ) {
+    return this.transacoesService.findAll(req.user);
+  }
 
-    @Delete(':id')
-      remove(@Param('id', ParseIntPipe) id: number, @Request() req) {
-        return this.transacoesService.remove(id, req.user);
-      }
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number, @Request() req) {
+    return this.transacoesService.remove(id, req.user);
+  }
 }
